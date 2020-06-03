45d38a444468241f47769ef55092193323cb5492
Nitzan Hen <nitzan.hen@agamimp.net>
2020-06-03 13:24:37 +0300

    write letters tests

--- src/alphabet/letters.ts
+++ src/alphabet/letters.ts
@@ -1,4 +1,4 @@
-import { stripDiacritics } from './diacritics';
+import diacritics, { stripDiacritics, Diacritic } from './diacritics';
 
 /**
  * List of Greek letters. 
@@ -37,9 +37,16 @@ const letters = {
 
 /**
  * @returns true if the string is a letter of the Greek alphabet, and false otherwise.
- * @param letter the string to be tested
+ * Can optionally test "strictly", meaning an accented letter (i.e. a letter with any diacritic symbol)
+ * will not count as a Greek letter.
+ * 
+ * @param letter the string to be tested.
+ * @param strict whether to test strictly or not, defaults to false. 
  */
-export function isGreekLetter(letter: string): boolean {
+export function isGreekLetter(letter: string, strict: boolean = false): boolean {
+    if (!strict) {
+        letter = stripDiacritics(letter);
+    }
     return letters.hasOwnProperty(letter);
 }
 
@@ -47,13 +54,34 @@ export function isGreekLetter(letter: string): boolean {
 /**
  * @returns true if the string is a sequence of letters of the Greek alphabet, 
  * and false otherwise. Note that spaces and puncuation marks are not letters.
- * @param text the test to be tested. 
+ * Can optionally test "strictly", meaning an accented text (i.e. a text with any diacritic symbol)
+ * will not count as Greek text.
+ * 
+ * @param letter the string to be tested.
+ * @param strict whether to test strictly or not, defaults to false. 
  */
-export function isGreekString(string: string): boolean {
-    return Array.from(string).every(isGreekLetter);
+export function isGreekString(string: string, strict: boolean = false): boolean {
+    return Array.from(string).every(letter => isGreekLetter(letter, strict));
 }
 
 //------ Vowels ------//
+
+
+/**
+ * Iota-subscript, unlike the other diacritics, makes a difference when it
+ * comes to determining whether a vowel is a monophthong or diphthong; all
+ * vowels with iota subscript are diphthongs.
+ * 
+ * the safeDiacritics array constains the keys of all diacritics except the iota
+ * subscript, and is used to strip only the safe diacritics in the useMonophthong()
+ * and useDiphthong() functions.
+ */
+const safeDiacritics = (() => {
+    const { iota_subscript, ...otherDiacritics } = diacritics;
+    return Object.keys(otherDiacritics) as Diacritic[];
+})();
+
+//Monophthongs 
 
 /**
  * Readonly collection of the monophthongs (single vowels) of the Greek alphabet,
@@ -67,6 +95,34 @@ export const monophtongs = Object.freeze(
 );
 
 /**
+ * Monophthong type; should match the possible values of the monophthongs array.
+ * Note that technically, the Monophthong type is not limited to the values below;
+ * using the isMonophthong() method, any *accented* variant of the union elements 
+ * can be inferred as a Monophthong.
+ */
+export type Monophthong = 'α' | 'ε' | 'η' | 'ι' | 'ο' | 'υ' | 'ω';
+
+/**
+ * @returns true if the passed letter param is in the monophthongs array, and false otherwise.
+ * Can optionally test "strictly", meaning an accented monophthong (i.e. a monophthong with any diacritic symbol)
+ * will not count as a monophthong.
+ * 
+ * @param letter the string to be tested. 
+ * @param strict whether to test strictly or not, defaults to false.
+ */
+export function isMonophthong(letter: string, strict: boolean = false): letter is Monophthong {
+    const { iota_subscript, ...otherDiacritics } = diacritics;
+
+    if (!strict) {
+        //DON'T remove iota subscript; all vowels with iota subscript are diphthongs
+        letter = stripDiacritics(letter, safeDiacritics);
+    }
+    return monophtongs.includes(letter);
+}
+
+//Diphthongs
+
+/**
  * Readonly collection of the diphthongs ("double vowels") of the Greek alphabet.
  * 
  * Note the difference between "αι" and "ᾳ"; the former is the proper diphthong, while the
@@ -74,11 +130,46 @@ export const monophtongs = Object.freeze(
  */
 export const diphthongs = Object.freeze(
     ["αι", "αυ", "ει", "ευ", "ηυ", "οι", "ου", "υι", "ᾳ", "ῃ", "ῳ"]
-)
+);
 
-export const vowels = Object.freeze(monophtongs.concat(diphthongs))
+/**
+ * Diphthong type; should match the possible values of the diphthongs array.
+ * Note that technically, the Diphthong type is not limited to the values below;
+ * using the isDiphthong() method, any *accented* variant of the union elements 
+ * can be inferred as a Diphthong.
+ */
+export type Diphthong = "αι" | "αυ" | "ει" | "ευ" | "ηυ" | "οι" | "ου" | "υι" | "ᾳ" | "ῃ" | "ῳ";
 
+/**
+ * @returns true if the passed letter param is in the diphthongs array, and false otherwise.
+ * Can optionally test "strictly", meaning an accented diphthongs (i.e. a diphthongs with any diacritic symbol)
+ * will not count as a diphthongs.
+ * 
+ * @param letter the string to be tested. 
+ * @param strict whether to test strictly or not, defaults to false.
+ */
+export function isDiphthong(letter: string, strict: boolean = false): letter is Diphthong {
+    if (!strict) {
+        letter = stripDiacritics(letter, safeDiacritics);
+    }
+    return diphthongs.includes(letter);
+}
 
+//Vowels
+
+/**
+ * Readonly collection of the vowels (monophthongs and diphthongs) 
+ * of the Greek alphabet.
+ */
+export const vowels = Object.freeze(monophtongs.concat(diphthongs));
+
+/**
+ * Vowel type; should match the possible values of the vowels array.
+ * Note that technically, the Vowel type is not limited to the values below;
+ * using the isVowel() method, any *accented* variant of the union elements
+ * can be inferred as a Vowel.
+ */
+export type Vowel = Monophthong | Diphthong;
 
 /**
  * @returns true if the passed letter param is in the vowels array, and false otherwise.
@@ -86,15 +177,13 @@ export const vowels = Object.freeze(monophtongs.concat(diphthongs))
  * will not count as a vowel.
  * 
  * @param letter the string to be tested. 
- * @param strict whether to test strictly or not, default to false.
+ * @param strict whether to test strictly or not, defaults to false.
  */
-export function isVowel(letter: string, strict: boolean = false): boolean {
-    if(!strict) {
-        //If we're not testing strictly, remove all diacritics.
-        letter = stripDiacritics(letter);
-    }
-    return vowels.includes(letter);
+export function isVowel(letter: string, strict: boolean = false): letter is Vowel {
+    return isMonophthong(letter, strict) || isDiphthong(letter, strict);
 }
+
+//------ Consonants ------//
 
 /**
  * Readonly collection of the consonants of the Greek alphabet,
@@ -108,12 +197,24 @@ export const consonants = Object.freeze(
 );
 
 /**
- * @returns true if the passed letter param is in the consonants array, and false otherwise.
- * @todo should the letter be normalized (stipped of accents, specifically for rho)? should separate methods exist
- * for testing normalized equality and strict equality?
- * @param letter the strring to be tested. 
+ * Consonant type; should match the possible values of the consonants array.
+ * Technically, a rho with rhough breathing also counts as a consonant (when testing
+ * with isConsonant(), the strict parameter being passed false), even though
+ * it's not present here.
  */
-export function isConsonant(letter: string): boolean {
+export type Consonant = 'β' | 'γ' | 'δ' | 'ζ' | 'θ' | 'κ' | 'λ' | 'μ' | 'ν' |
+    'ξ' | 'π' | 'ρ' | 'σ' | 'τ' | 'φ' | 'χ' | 'ψ';
+
+/**
+ * @returns true if the passed letter param is in the consonants array, and false otherwise.
+ * A rho with rough breathing counts as a consonants, and passing it to this method returns true.
+ * 
+ * @param letter the string to be tested. 
+ */
+export function isConsonant(letter: string): letter is Consonant {
+    //Remove rough breathing mark in case the letter is rho
+    letter = stripDiacritics(letter, ["rough_breathing"]);
+
     return consonants.includes(letter);
 }
 
--- tests/letters.test.ts
+++ tests/letters.test.ts
@@ -1,14 +1,108 @@
-import letters, { vowels, consonants } from '../src/alphabet/letters';
+import letters, { vowels, consonants, monophtongs, diphthongs, isMonophthong, isDiphthong, isVowel, isGreekLetter } from '../src/alphabet/letters';
+import diacritics from '../src/alphabet/diacritics';
 
 describe("lettes.ts", () => {
-    test("letters, vowels & consonants objects", () => {
+    test("All letter groups have the correct number of elements", () => {
+        //There should be exactly 24 letters
         expect(Object.keys(letters)).toHaveLength(24);
+
+        //Each letter needs to be either a vowel or a consonant
         Object.values(letters).forEach(({ type }) => {
-            expect(["vowel", "consonant"]).toContain(type)
-        })
-        expect(Object.keys(vowels)).toHaveLength(7);
-        expect(Object.keys(consonants)).toHaveLength(17);
+            expect(["vowel", "consonant"]).toContain(type);
+        });
+
+        //There should be exactly 7 monophthongs, 11 diphthongs, 18 vowels total and 17 consonants
+        expect(monophtongs).toHaveLength(7);
+        expect(diphthongs).toHaveLength(11);
+        expect(vowels).toHaveLength(18);
+        expect(consonants).toHaveLength(17);
     });
 
-    /** @todo add tests for isVowel(), isGreekLetter(), etc. with accented letter tests. */
+    test("isMonophthong(), isDiphthong(), isVowel() work as expected", () => {
+        const { iota_subscript } = diacritics;
+        //Test data
+        const monophthongTests = ['α', 'ε', 'η', 'ι', 'ο', 'υ', 'ω'];
+        const diphthongTests = ["αι", "αυ", "ει", "ευ", "ηυ", "οι", "ου", "υι", "ᾳ", "ῃ", "ῳ"];
+        const nonGreekTests = ['a', 'o', 'nῳ', 'αa', 'א'];
+        const emptyTest = "";
+        const nonStrictMonophthongTests = ['ά', 'ῶ', 'ἡ', 'ὄ'];
+        const nonStrictDiphthongTests = ['οὐ', 'οῖ', 'ῷ', 'αὶ', 'εἰ', 'οῦ'];
+        const malformedTests = ['ῦῦ', 'ιῷ', 'ι' + iota_subscript, 'εια', "ια", 'ωε', 'οο', 'ου' + iota_subscript];
+
+        monophthongTests.forEach(mono => {
+            expect(isMonophthong(mono)).toBe(true);
+            expect(isDiphthong(mono)).toBe(false);
+            expect(isVowel(mono)).toBe(true);
+            expect(isGreekLetter(mono)).toBe(true);
+
+            //Strict mode passes too
+            expect(isMonophthong(mono, true)).toBe(true);
+            expect(isDiphthong(mono, true)).toBe(false);
+            expect(isVowel(mono, true)).toBe(true);
+            expect(isGreekLetter(mono, true)).toBe(true);
+        });
+
+        diphthongTests.forEach(diph => {
+            console.log(diph, isGreekLetter(diph))
+            expect(isMonophthong(diph)).toBe(false);
+            expect(isDiphthong(diph)).toBe(true);
+            expect(isVowel(diph)).toBe(true);
+
+            //Strict mode passes too
+            expect(isMonophthong(diph, true)).toBe(false);
+            expect(isDiphthong(diph, true)).toBe(true);
+            expect(isVowel(diph, true)).toBe(true);
+            expect(isGreekLetter(diph, true)).toBe(false);
+        });
+
+        nonGreekTests.forEach(text => {
+            expect(isMonophthong(text)).toBe(false);
+            expect(isDiphthong(text)).toBe(false);
+            expect(isVowel(text)).toBe(false);
+            expect(isGreekLetter(text)).toBe(false);
+        });
+
+        expect(isMonophthong(emptyTest)).toBe(false);
+        expect(isDiphthong(emptyTest)).toBe(false);
+        expect(isVowel(emptyTest)).toBe(false);
+        expect(isGreekLetter(emptyTest)).toBe(false);
+
+        nonStrictMonophthongTests.forEach(test => {
+            //Non-strict mode works fine
+            expect(isMonophthong(test)).toBe(true);
+            expect(isDiphthong(test)).toBe(false);
+            expect(isVowel(test)).toBe(true);
+            expect(isGreekLetter(test)).toBe(true);
+
+            //Strict mode returns false
+            expect(isMonophthong(test, true)).toBe(false);
+            expect(isDiphthong(test, true)).toBe(false);
+            expect(isVowel(test, true)).toBe(false);
+            expect(isGreekLetter(test, true)).toBe(false);
+        });
+
+        nonStrictDiphthongTests.forEach(test => {
+            //Non-strict mode works fine
+            expect(isMonophthong(test)).toBe(false);
+            expect(isDiphthong(test)).toBe(true);
+            expect(isVowel(test)).toBe(true);
+
+            //Strict mode returns false
+            expect(isMonophthong(test, true)).toBe(false);
+            expect(isDiphthong(test, true)).toBe(false);
+            expect(isVowel(test, true)).toBe(false);
+            expect(isGreekLetter(test, true)).toBe(false);
+        });
+
+        malformedTests.forEach(test => {
+            expect(isMonophthong(test)).toBe(false);
+            expect(isDiphthong(test)).toBe(false);
+            expect(isVowel(test)).toBe(false);
+
+            expect(isMonophthong(test, true)).toBe(false);
+            expect(isDiphthong(test, true)).toBe(false);
+            expect(isVowel(test, true)).toBe(false);
+            expect(isGreekLetter(test, true)).toBe(false);
+        });
+    });
 });
