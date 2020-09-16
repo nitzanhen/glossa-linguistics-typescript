41839f8941f4fa98e26d745abe142817c2412505
NitzanHen <nitzan.alpha@gmail.com>
2020-09-16 23:10:50 +0300

    bit of adjective inffuncs work

--- src/service/inflection/function/adjective/AdjectiveInflectionFunctions.ts
+++ src/service/inflection/function/adjective/AdjectiveInflectionFunctions.ts
@@ -1,12 +1,92 @@
+import { default as inflectionFunction } from '../../composition/composeAdjectiveInflectionFunction';
 
 const adjectiveInflectionFunctions = {
     second_declension: {
-        two_suffixed: {
-
-        },
         three_suffixed: {
+            masculine: {
+                singular: {
+                    nominative: inflectionFunction("ος"),
+                    genitive: inflectionFunction("ου"),
+                    dative: inflectionFunction("ῳ"),
+                    accusative: inflectionFunction("ον"),
+                    vocative: inflectionFunction()
+                },
+                plural: {
+                    nominative: inflectionFunction("η"),
+                    genitive: inflectionFunction("ης"),
+                    dative: inflectionFunction("ῃ"),
+                    accusative: inflectionFunction(),
+                    vocative: inflectionFunction()
+                }
+            },
+            feminine: {
+                singular: {
+                    nominative: inflectionFunction(),
+                    genitive: inflectionFunction(),
+                    dative: inflectionFunction(),
+                    accusative: inflectionFunction(),
+                    vocative: inflectionFunction()
+                },
+                plural: {
+                    nominative: inflectionFunction(),
+                    genitive: inflectionFunction(),
+                    dative: inflectionFunction(),
+                    accusative: inflectionFunction(),
+                    vocative: inflectionFunction()
+                }
+            },
+            neuter: {
+                singular: {
+                    nominative: inflectionFunction(),
+                    genitive: inflectionFunction(),
+                    dative: inflectionFunction(),
+                    accusative: inflectionFunction(),
+                    vocative: inflectionFunction()
+                },
+                plural: {
+                    nominative: inflectionFunction(),
+                    genitive: inflectionFunction(),
+                    dative: inflectionFunction(),
+                    accusative: inflectionFunction(),
+                    vocative: inflectionFunction()
+                }
+            }
+        },
+        two_suffixed: {
+            masculine_feminine: {
+                singular: {
+                    nominative: inflectionFunction(),
+                    genitive: inflectionFunction(),
+                    dative: inflectionFunction(),
+                    accusative: inflectionFunction(),
+                    vocative: inflectionFunction()
+                },
+                plural: {
+                    nominative: inflectionFunction(),
+                    genitive: inflectionFunction(),
+                    dative: inflectionFunction(),
+                    accusative: inflectionFunction(),
+                    vocative: inflectionFunction()
+                }
+            },
+            neuter: {
+                singular: {
+                    nominative: inflectionFunction(),
+                    genitive: inflectionFunction(),
+                    dative: inflectionFunction(),
+                    accusative: inflectionFunction(),
+                    vocative: inflectionFunction()
+                },
+                plural: {
+                    nominative: inflectionFunction(),
+                    genitive: inflectionFunction(),
+                    dative: inflectionFunction(),
+                    accusative: inflectionFunction(),
+                    vocative: inflectionFunction()
+                }
+            }
+        },
 
-        }
     },
     third_declension: {
 
